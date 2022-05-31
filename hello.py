c2b35f059b42f3f41a40584e52978d7a3aa4599c
afapura <aluno81664@ipt.pt>
2022-05-31 16:33:33 +0100

    First commit

---
+++ hello.py
@@ -0,0 +1,4 @@
+def say_hi():
+    print('Hi, my name is Afonso Apura.')
+
+__version__ = '0.1'
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='hello',
+      version='1.0',
+      description='Hello Distribution Module',
+      author='Afonso Apura',
+      author_email='aluno81664@ipt.pt',
+      url='https://github.com/afapura/test',
+      py_modules=['hello'],
+     )
